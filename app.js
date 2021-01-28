d4734a826b686ccfca1dc0f956e20ccd80b84bd6
HC <manlychandler@gmail.com>
2021-01-28 10:18:46 -0500

    drafted js third party api code but still not pulling the correct data

--- app.js
+++ app.js
@@ -1,10 +1,66 @@
-// API Key: 7978f186187864ed7fc387296b8ece2c
-// Endpoint: api.openweathermap.org
-// API Call By City Name: api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key} 
-
 const API_KEY = '7978f186187864ed7fc387296b8ece2c'
+const CITY_NAME = 'Atlanta'
 
 const getCity = async () => {
-  let response = await axios.get(`https://cors-anywhere.herokuapp.com/https://api.openweathermap.org/data/2.5/weather?q=Boston&appid=${API_KEY}`)
-  console.log(response)
+  try {
+    let response = await axios.get(`https://cors-anywhere.herokuapp.com/https://api.openweathermap.org/data/2.5/weather?q=${CITY_NAME}&appid=${API_KEY}`)
+    // console.log(response)
+    let cityList = Object(response.data)
+    console.log(cityList)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+getCity()
+
+const getOptions = (list) => {
+  // console.log(list)
+  let select = document.querySelector('#city-name')
+  console.log(select)
+  list.forEach(city => {
+    let option = document.createElement('option')
+    option.value = city
+    option.textContent = city
+    // console.log(option)
+    select.append(option)
+  })
+}
+
+const getValue = (e) => {
+  e.preventDefault()
+  let optionValue = document.querySelector('#city-name').value
+  // console.log(optionValue)
+  removeWeather()
+  getCity(optionValue)
+}
+
+const form = document.querySelector('form')
+
+form.addEventListener('search', getValue)
+
+const getWeather = async (weather) => {
+  try {
+    let response = await axios.get(`https://cors-anywhere.herokuapp.com/https://api.openweathermap.org/data/2.5/weather?q=${CITY_NAME}&appid=${API_KEY}`)
+    // console.log(response)
+    getWeather(response)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+getWeather()
+
+const currentWeather = (city) => {
+  let weatherData = document.createElement('p')
+  image.src = weather
+  let div = document.querySelector('#append-weather')
+  div.append(image)
+}
+
+const removeWeather = () => {
+  let weatherDiv = document.querySelector('#append-weather')
+  while (weatherDiv.lastChild) {
+    weatherDiv.removeChild(weatherDiv.lastChild)
+  }
 }
