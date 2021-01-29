c93e154f20e006a64dcad46be52f00d8247a7160
HC <manlychandler@gmail.com>
2021-01-29 11:11:55 -0500

    added removal refresh functionality

--- app.js
+++ app.js
@@ -6,28 +6,24 @@ let appendWeather = document.getElementById('append-weather')
 
 const getCity = async () => {
   try {
+    removeWeatherData()
     let response = await axios.get(`https://cors-anywhere.herokuapp.com/https://api.openweathermap.org/data/2.5/weather?q=${input.value}&units=imperial&appid=${API_KEY_TWO}`)
-    // console.log(response)
+    console.log(response)
     let weatherData = response.data.main
-    appendWeather.innerHTML += `<div>The temperature in ${input.value} is ${weatherData.temp + `°F`}</div>`
+    appendWeather.innerHTML += `<div>The current temperature in ${input.value} is ${weatherData.temp + `°F`}</div>`
   } catch (error) {
     console.log(error)
   }
 }
 
-getCity()
-
-// const getValue = (e) => {
-//   e.preventDefault()
-//   getCity()
-// }
 
 const button = document.querySelector('button')
 button.addEventListener('click', getCity)
 
 function removeWeatherData() {
-  while (appendWeather.length > -1) {
-    appendWeather--;
+  const weatherContainer = document.querySelector('#append-weather')
+  while (weatherContainer.lastChild) {
+    weatherContainer.removeChild(weatherContainer.lastChild);
   }
-  removeWeatherData()
 }
+
