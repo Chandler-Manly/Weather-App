961e152fab02d492dd79acd1e840dcb619023091
HC <manlychandler@gmail.com>
2021-01-28 20:31:04 -0500

    added js functionality to retrieve weather data

--- app.js
+++ app.js
@@ -1,36 +1,31 @@
 // const API_KEY = '7978f186187864ed7fc387296b8ece2c'
 const API_KEY_TWO = '1a012b94443d511ac871535b64ac2706'
+const input = document.querySelector('input')
 
+let appendWeather = document.getElementById('append-weather')
 
 const getCity = async () => {
   try {
-    const CITY_NAME = document.querySelector('#city-name')
-    let response = await axios.get(`https://cors-anywhere.herokuapp.com/https://api.openweathermap.org/data/2.5/weather?q=${CITY_NAME.value}&units=imperial&appid=${API_KEY_TWO}`)
-    // console.log(response)
+    let response = await axios.get(`https://cors-anywhere.herokuapp.com/https://api.openweathermap.org/data/2.5/weather?q=${input.value}&units=imperial&appid=${API_KEY_TWO}`)
+    console.log(response)
     let weatherData = response.data.main
-    // console.log(weatherData)
+    // removeWeatherData()
+    appendWeather.innerHTML += `<div>The temperature in ${input.value} is ${weatherData.temp + `°F`}</div>`
   } catch (error) {
     console.log(error)
   }
 }
+
+getCity()
 
 const getValue = (e) => {
   e.preventDefault()
   getCity()
 }
 
+const button = document.querySelector('button')
+button.addEventListener('click', getCity)
 
-function showWeatherData(response.data.main) {
-  const weatherInfo = `<h2> City Name: ${data.name}</h2>`
-  let dataContainer = document.querySelector('#append-weather')
-  dataContainer.insertAdjacentHTML('beforeend', weatherInfo)
-}
+// removeWeatherData(){
 
-const form = document.querySelector('form')
-form.addEventListener('submit', getValue)
-
-showWeatherData()
-
-// removeWeatherData (){}
-// write code here 
-// make sure to call function 
+// }
